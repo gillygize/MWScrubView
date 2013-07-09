139062c471292c85da2c856ff81c8d3380342891
Matthew Gillingham <me@mattgillingham.com>
2013-07-09 16:22:09 +0900

    Initial Commit

---
+++ MWScrubCollectionVIew/MWAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  MWAppDelegate.h
+//  MWScrubCollectionVIew
+//
+//  Created by Matt on 7/9/13.
+//  Copyright (c) 2013 Matthew Gillingham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class MWViewController;
+
+@interface MWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) MWViewController *viewController;
+
+@end
---
+++ MWScrubCollectionVIew/MWViewController.h
@@ -0,0 +1,13 @@
+//
+//  MWViewController.h
+//  MWScrubCollectionVIew
+//
+//  Created by Matt on 7/9/13.
+//  Copyright (c) 2013 Matthew Gillingham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MWViewController : UIViewController
+
+@end
